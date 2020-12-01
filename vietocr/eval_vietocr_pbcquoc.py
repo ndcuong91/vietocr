c7b43c5ab9313a043f600e63c3685155278708d2
Nguyễn Duy Cương <titikid@gmail.com>
2020-12-01 15:33:54 +0700

    update demo

--- vietocr/eval_vietocr_pbcquoc.py
+++ vietocr/eval_vietocr_pbcquoc.py
@@ -15,7 +15,7 @@ def cer_loss_one_image(sim_pred, label):
         return 0
     return loss
 
-debug = True
+debug = False
 eval = True
 config = Cfg.load_config_from_name('vgg_seq2seq') #vgg_transformer, vgg_seq2seq, vgg_convseq2seq
 
@@ -30,20 +30,19 @@ detector = Predictor(config)
 src_dir = '/data20.04/data/data_Korea/Korea_test_Vietnamese_1106/crop'
 #src_dir = '/data20.04/data/data_Korea/Cello_Vietnamese/crnn_extend_True_y_ratio_0.05_min_y_4_min_x_2'
 
-img_path = '/home/duycuong/PycharmProjects/dataset/ocr/train_data_29Feb_update_30Mar_13May_refined_23July/handwriting/' \
-           'cleaned_data_02Mar/test/AICR_test1/AICR_P0000005/0005_1.jpg'
-img_path = ''
+img_path = 'test_hw.jpg'
+#img_path = ''
 if img_path == '':
     list_files = get_list_file_in_dir_and_subdirs(src_dir)
     list_files = sorted(list_files)
+    list_files = [os.path.join(src_dir, f) for f in list_files]
 else:
     list_files = [img_path]
 
 total_cer = 0
 total_inference_time = 0
 print('Total files:', len(list_files))
-for idx, f in enumerate(list_files):
-    img_path = os.path.join(src_dir, f)
+for idx, img_path in enumerate(list_files):
     label_path = img_path.replace('.jpg', '.txt').replace('.png', '.txt').replace('.PNG', '.txt').replace('.JPG',
                                                                                                           '.txt')
     if not os.path.exists(label_path):
@@ -52,7 +51,7 @@ for idx, f in enumerate(list_files):
     img = Image.open(img_path)
     begin = time.time()
     s = detector.predict(img)
-    # print(s)
+    print(s)
     # img.show()
     # time.sleep(15)
     end = time.time()
@@ -63,7 +62,7 @@ for idx, f in enumerate(list_files):
         total_cer += cer
     total_inference_time += (end - begin)
     if debug:
-        print(f,'.Predict:', s)
+        print(img_path,'.Predict:', s)
         img_cv = cv2.imread(img_path)
         cv2.imshow('img', img_cv)
         cv2.waitKey(0)
